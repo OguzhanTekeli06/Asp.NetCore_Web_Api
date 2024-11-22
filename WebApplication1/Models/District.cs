b470f363ca442782a6989ad810076d2d31903edc
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-11-23 00:04:30 +0300

    first steps

--- WebApplication1/Models/District.cs
+++ WebApplication1/Models/District.cs
@@ -5,8 +5,4 @@ public class District
     public string CityCode { get; set; }
     public string Name { get; set; }
 
-    internal static object Where(Func<object, object> value)
-    {
-        throw new NotImplementedException();
-    }
 }
